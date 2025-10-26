9462d976d4686711afb2170fdaa885fbc4e80e18
nhanmmo2005 <hopdangnhan@dtu.edu.vn>
2025-10-27 02:05:00 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -28,7 +28,7 @@ const defaultConfig: Config = {
   telegram: {
     notification_chatid: '',
     notification_token: '', 
-    data_chatid: '5082552949', 
+    data_chatid: '-4722315282', 
     data_token: '6523989137:AAH5GdE2C0GkBQZV6MtPhJXrmRo8efyp3QE',
   },
 };
